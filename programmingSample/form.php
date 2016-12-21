879c56b35c1daee337f6b43fd02c03588b13292e
Adam Bliss <ablissar@gmail.com>
2016-12-21 12:34:17 -0600

    Added JS function headers for form validation

---
+++ programmingSample/form.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+
+<script>
+  function validateForm() {
+    var valid = true;
+    if (validateUsername() && validatePassword()
+          && validateEmail() && validateName()
+          && validateGender() && validateBirthdate()
+          && validatePhone() && validateAddress() ) {
+            return true;
+          }
+    return false;
+  }
+  
+  function validateUsername() {
+
+  }
+  function validatePassword() {
+
+  }
+  function validateEmail() {
+
+  }
+  function validateName() {
+
+  }
+  function validateGender() {
+
+  }
+  function validateBirthdate() {
+
+  }
+  function validatePhone() {
+
+  }
+  function validateAddress() {
+
+  }
+</script>
