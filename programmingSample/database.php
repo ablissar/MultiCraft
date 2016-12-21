8f6273d2975aa4186d6ceb58addd811df0f57b8e
Adam Bliss <ablissar@gmail.com>
2016-12-21 12:54:36 -0600

    Started on client-side validation

---
+++ programmingSample/database.php
@@ -0,0 +1,9 @@
+<?php
+    // Server-side validation
+    
+    // At this point, the data has been validated on both
+    // the client and server side and is ready to be
+    // entered into the database.
+
+    // === Database code here ===
+ ?>
--- programmingSample/form.php
+++ programmingSample/form.php
@@ -1,22 +1,44 @@
 <!DOCTYPE HTML>
 
 <script>
-  function validateForm() {
+    // Client-side validation
+    function validateForm() {
     var valid = true;
     if (validateUsername() && validatePassword()
-          && validateEmail() && validateName()
-          && validateGender() && validateBirthdate()
-          && validatePhone() && validateAddress() ) {
+        && validateEmail() && validateName()
+        && validateGender() && validateBirthdate()
+        && validatePhone() && validateAddress() ) {
             return true;
-          }
+    }
     return false;
   }
-  
+
   function validateUsername() {
-
+      if( isset(document.forms["form"]["username"]) ) {
+          var username = document.forms["form"]["username"];
+          // Checks that username is appropriate length and contains no spaces
+          if( username.length > 20 || username.length < 4 || username.includes(' ') ) {
+              return false;
+          }
+          return true;
+      }
+      return false;
   }
-  function validatePassword() {
 
+  function validatePassword() {
+      if( isset(document.forms["form"]["password"]) ) {
+          var password = document.forms["form"]["password"];
+          // Checks that password is appropriate length, meets
+          // basic security standards, and matches confirmation password
+          if( password.lenth < 8 ) {
+              return false;
+          }
+          if( password != document.forms["form"]["passwordConfirm"] ) {
+              return false;
+          }
+          return true;
+      }
+      return false;
   }
   function validateEmail() {
 
